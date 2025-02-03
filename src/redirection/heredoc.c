f5fa533851d56896e103ed115fbc8ff366aa4d17
Alex <momiji.kling@gmail.com>
2025-02-03 21:15:48 +0100

    normed assemble heredoc function

--- src/redirection/heredoc.c
+++ src/redirection/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: mkling <mkling@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 15:42:30 by mkling            #+#    #+#             */
-/*   Updated: 2025/02/03 18:28:35 by mkling           ###   ########.fr       */
+/*   Updated: 2025/02/03 18:36:35 by mkling           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@
 
 /* FILE METHOD */
 
-char	*generate_heredoc_filepath(t_shell *shell)
+static char	*generate_heredoc_filepath(t_shell *shell)
 {
 	static int	i;
 	char		*itoa_i;
@@ -69,10 +69,28 @@ char	*generate_heredoc_filepath(t_shell *shell)
 	return (heredoc_path);
 }
 
+static int	accumulate_heredoc_content(int heredoc_fd, char *end_of_file)
+{
+	char	*line;
+
+	while (1)
+	{
+		write(STDIN_FILENO, "> ", 3);
+		line = get_next_line(STDIN_FILENO);
+		if (ft_strncmp(end_of_file, line, ft_strlen(end_of_file)) == 0)
+		{
+			free(line);
+			break ;
+		}
+		write(heredoc_fd, line, ft_strlen(line));
+		free(line);
+	}
+	return (SUCCESS);
+}
+
 int	assemble_heredoc(t_shell *shell, t_cmd *cmd, char *end_of_file)
 {
 	char	*heredoc_filepath;
-	char	*result;
 	int		fd;
 
 	heredoc_filepath = generate_heredoc_filepath(shell);
@@ -81,18 +99,7 @@ int	assemble_heredoc(t_shell *shell, t_cmd *cmd, char *end_of_file)
 	fd = open(heredoc_filepath, O_RDWR | O_TRUNC | O_CREAT, 777);
 	if (fd < 0)
 		return (set_cmd_error(OPEN_ERROR, cmd, "Heredoc"), OPEN_ERROR);
-	while (1)
-	{
-		write(STDIN_FILENO, "> ", 3);
-		result = get_next_line(STDIN_FILENO);
-		if (ft_strncmp(end_of_file, result, ft_strlen(end_of_file)) == 0)
-		{
-			free(result);
-			break ;
-		}
-		write(fd, result, ft_strlen(result));
-		free(result);
-	}
+	accumulate_heredoc_content(fd, end_of_file);
 	close(fd);
 	cmd->fd_in = open(heredoc_filepath, O_RDONLY);
 	if (cmd->fd_in < 0)
